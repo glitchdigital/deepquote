069dd38232535e1bab895572faa06e85ba41f879
Iain Collins <me@iaincollins.com>
2020-02-20 13:40:04 +0000

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const path = require('path')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
-module.exports = {
+module.exports = withBundleAnalyzer({
   webpack: (config) => {
     config.resolve.modules.unshift(path.resolve('./'))
     // Fixes npm packages that depend on `fs` module
     config.node = { fs: 'empty' }
     return config
   }
-}
+})
