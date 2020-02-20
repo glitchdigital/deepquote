e4a99cc522c84f50fc00e447ac65b16404a65e64
Iain Collins <me@iaincollins.com>
2020-02-20 03:01:00 +0000

    Reduce CSS bundle size with PurgeCSS
    Reduced first paint times in standard Lighthouse tests by 0.8s, reduced CSS bundle size and increase performance score by 4 points.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,21 @@
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: ["./components/**/*.js", "./pages/**/*.js"],
+    whitelist: ['-zondicon'],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  }
+]
 module.exports = {
   plugins: [
-    'tailwindcss',
+    'postcss-import',
     'postcss-nested',
-    'postcss-preset-env'
+    'postcss-preset-env',
+    'tailwindcss',
+    'autoprefixer',
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ],
 }
+
+//whitelist: ['-zondicon']
+//whitelistPatterns: [/-ml-48$/]
