98479c5375511fcc0eae8c5e094b36fdfbe554fd
Iain Collins <me@iaincollins.com>
2020-02-24 19:20:41 +0000

    Add app version to footer on homepage
    This is to make it easy to check what version is running in production.

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'components/head'
 import Nav from 'components/nav'
 import QuoteCard from 'components/cards/quote'
 import { useFetch, useFetchSync, HOSTNAME } from 'components/fetch-hook'
+import { version } from 'package.json'
 
 const QUOTES_API_ENDPOINT = '/api/quotes'
 
@@ -20,8 +21,14 @@ const Page = (props) => {
         <h1 className='mb-2 mt-10 px-2 md:px-0 text-5xl md:text-6xl font-serif'>Did they really say that?</h1>
         <p className='text-lg md:text-3xl text-gray-600 mb-10'>Find the earliest evidence of a quote</p>
       </div>
-      <div className='mt-5 grid lg:grid-cols-3 pl-2 pr-2 mb-2 bg-gray-100 border-t pt-2' style={{minHeight: '500px'}}>
+      <div className='mt-4 grid lg:grid-cols-3 pl-2 pr-2 mb-2 bg-gray-100 border-t pt-2' style={{minHeight: '500px'}}>
         {quotes.map((quote) => <QuoteCard key={quote.hash} {...quote}/> )}
+      </div>
+      <div className="mt-4">
+        <hr/>
+        <p className="text-sm text-center p-4">
+          Version {version}
+        </p>
       </div>
     </>
   )
