8f9ae4481e0209cc3a07b75d6b356c4b45467a8f
Iain Collins <me@iaincollins.com>
2020-02-25 20:14:14 +0000

    Add fade effect as quote loads in
    This is to alleviate elements 'popping' in in a jarring way when rendering client side.

--- pages/quote/[id].js
+++ pages/quote/[id].js
@@ -27,43 +27,45 @@ const Page = (props) => {
       <Head title={quote?.text ? `"${quote.text}"` : ''} url={url}/>
       <Nav />
       <div className='bg-gray-100 fixed top-0 w-full h-screen z-0'/>
-      {quote &&
-        <div className='pt-5 sm:pt-10 pb-5'>
-          <div className='relative m-auto px-5 max-w-screen-md'>
-            <div className='relative rounded-lg bg-gray-300 z-30'>
-              <Quote {...quote}/>
-              <div className='absolute right-0 text-sm font-semibold rounded-full border shadow-sm' style={{bottom: '-50px'}}>
-                <Link href={`/quote/[id]`} as={`/quote/${quote?.hash}`}>
-                  <a className={classname('text-gray-600 inline-block no-underline hover:underline rounded-l-full px-4 py-2 hover:bg-white bg-gray-100', { 'bg-gray-200 hover:bg-gray-200 text-gray-800 shadow-inner': !showAll })}>
-                    Exact matches
-                  </a>
-                </Link>
-                <Link href={`/quote/[id]?showAll=true`} as={`/quote/${quote?.hash}?showAll=true`}>
-                  <a className={classname('text-gray-600 inline-block no-underline hover:underline rounded-r-full px-4 py-2 hover:bg-white bg-gray-100', { 'bg-gray-200 hover:bg-gray-200 text-gray-800 shadow-inner': showAll })}>
-                    Possible matches
-                  </a>
-                </Link>
+      <div className={classname('transition-all ease-in-out duration-500 relative overflow-hidden', quote ? 'opacity-1' : 'opacity-0')}>
+        {quote &&
+          <div className='pt-5 sm:pt-10 pb-5'>
+            <div className='relative m-auto px-5 max-w-screen-md'>
+              <div className='relative rounded-lg bg-gray-300 z-30'>
+                <Quote {...quote}/>
+                <div className='absolute right-0 text-sm font-semibold rounded-full border shadow-sm' style={{bottom: '-50px'}}>
+                  <Link href={`/quote/[id]`} as={`/quote/${quote?.hash}`}>
+                    <a className={classname('text-gray-600 inline-block no-underline hover:underline rounded-l-full px-4 py-2 hover:bg-white bg-gray-100', { 'bg-gray-200 hover:bg-gray-200 text-gray-800 shadow-inner': !showAll })}>
+                      Exact matches
+                    </a>
+                  </Link>
+                  <Link href={`/quote/[id]?showAll=true`} as={`/quote/${quote?.hash}?showAll=true`}>
+                    <a className={classname('text-gray-600 inline-block no-underline hover:underline rounded-r-full px-4 py-2 hover:bg-white bg-gray-100', { 'bg-gray-200 hover:bg-gray-200 text-gray-800 shadow-inner': showAll })}>
+                      Possible matches
+                    </a>
+                  </Link>
+                </div>
+              </div>
+              <div className='relative pt-6'>
+                <div className='absolute flex z-10' style={{
+                    width: '10px',
+                    top: -40,
+                    bottom: 0,
+                    left: 48,
+                    borderWidth: '0 0 0 5px'
+                  }}/>
+                <div className='absolute w-full bg-gray-100 z-20' style={{
+                    bottom: 0,
+                    height: 140,
+                  }}/>
+                {quote?.citations?.filter((citation) => (showAll || !citation.suggestedResult)).map((citation, i) =>
+                  <Citation key={citation.url} {...citation} position={i+1}/>
+                )}
               </div>
             </div>
-            <div className='relative pt-6'>
-              <div className='absolute flex z-10' style={{
-                  width: '10px',
-                  top: -40,
-                  bottom: 0,
-                  left: 48,
-                  borderWidth: '0 0 0 5px'
-                }}/>
-              <div className='absolute w-full bg-gray-100 z-20' style={{
-                  bottom: 0,
-                  height: 140,
-                }}/>
-              {quote?.citations?.filter((citation) => (showAll || !citation.suggestedResult)).map((citation, i) =>
-                <Citation key={citation.url} {...citation} position={i+1}/>
-              )}
-            </div>
           </div>
+        }
       </div>
-      }
     </>
   )
 }
