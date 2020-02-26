7d8cd1c0cceaf824dddbe7f48975807838cf6f67
Iain Collins <me@iaincollins.com>
2020-02-26 13:32:41 +0000

    Improve nav on mobile
    * Improved padding
    * Now sets focus on search input when menu is opened
    
    This reduces the amount of taps/clicks required to perform the same action on mobile.
    
    Ideally it would make sense to also add a search option, but that requires further work.

--- components/nav.js
+++ components/nav.js
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Router from 'next/router'
 import Link from 'next/link'
 import classname from 'classname'
 import { Home, Search } from 'react-zondicons'
 
 export default ({defaultSearchText}) => {
+  const searchInput = useRef()
   const [menuOpenState, setMenuOpenState] = useState(false)
-  const toggleMenuOpenState = () => setMenuOpenState(!menuOpenState)
+  const toggleMenuOpenState = () => {
+    if (!menuOpenState) setTimeout(() => searchInput.current.focus(), 100)
+    setMenuOpenState(!menuOpenState)
+  }
 
   const [searchText = '', setSearchText] = useState(defaultSearchText)
   const onSearchTextChange = event => setSearchText(event.target.value)
@@ -28,7 +32,7 @@ export default ({defaultSearchText}) => {
         </a>
       </Link>
       <div className='sm:hidden'>
-        <button aria-label='Menu' onClick={toggleMenuOpenState} type='button' className='block rounded-full p-1 hover:bg-gray-100 active:bg-gray-100 active:shadow-lg text-gray-500 hover:text-gray-600 focus:text-gray-600 focus:outline-none'>
+        <button aria-label='Menu' onClick={toggleMenuOpenState} type='button' className='float-right inline-block rounded-full p-1 hover:bg-gray-100 active:bg-gray-100 active:shadow-lg text-gray-600 hover:text-gray-800 focus:text-gray-600'>
           <svg className='h-6 w-6 fill-current' viewBox='0 0 24 24' alt='Menu' aria-hidden='true'>
             <path className={classname({ 'hidden': menuOpenState })} fillRule='evenodd' d='M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z'/>
             <path className={classname({ 'hidden': !menuOpenState })} fillRule='evenodd' d='M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z'/>
@@ -40,6 +44,8 @@ export default ({defaultSearchText}) => {
       <form className='relative' onSubmit={onSearchFormSubmit}>
         <Search className='text-gray-500 pointer-events-none' style={{position: 'absolute', left: 14, top: 12}}/>
         <input
+          id='searchInput'
+          ref={searchInput}
           aria-label='Search'
           autoComplete='off'
           spellCheck='false'
@@ -49,9 +55,9 @@ export default ({defaultSearchText}) => {
           onChange={onSearchTextChange}
           />
       </form>
-      <a href='#' className='border-2 border-transparent mt-0 sm:mt-1 sm:ml-2'>Who we are</a>
-      <a href='#' className='border-2 border-transparent mt-0 sm:mt-1 sm:ml-2'>FAQ</a>
-      <a href='#' className='border-2 border-transparent mt-0 sm:mt-1 sm:ml-2'>Impressum</a>
+      <a href='#' className='border-2 border-transparent mt-1 sm:ml-2'>Who we are</a>
+      <a href='#' className='border-2 border-transparent mt-1 sm:ml-2'>FAQ</a>
+      <a href='#' className='border-2 border-transparent mt-1 sm:ml-2'>Impressum</a>
     </nav>
   </header>
   )
